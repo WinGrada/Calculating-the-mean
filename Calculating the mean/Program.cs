262e1815daae557e7a0c9c3a789a599666b5c77e
Wingrada <wingrada@gmail.com>
2020-07-16 12:59:40 +0500

    Add project files.

---
+++ Calculating the mean/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Calculating_the_mean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+            // цикл получение чисел
+            nums = GetUserNumbers(nums);
+            int meanNum = CalculatingMean(nums);
+            Console.WriteLine(meanNum);
+
+        }
+
+        static int[] GetUserNumbers(int [] nums)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+                if (nums[i] == 0) break;
+            }
+            return nums;
+        }
+        static int CalculatingMean(int [] nums )
+        {
+            int totalNum = 0;
+            foreach (int num in nums)
+            {
+                totalNum += num;
+            }
+            totalNum /= 3;
+
+            return totalNum;
+        }
+    }
+}
